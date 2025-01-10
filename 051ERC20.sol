0ed7956163d31cda7488fcf68e8a7f9ad3c3d7f7
q1195106845 <q1195106845@163.com>
2025-01-10 12:47:14 +0800

    ERC20 基础实现

---
+++ 051ERC20.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
+
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+contract ERC20 is IERC20 {
+    string public name = "TestToken";
+
+    string public symbol = "TEST";
+
+    uint8 public decimals = 18;
+
+    uint256 public override totalSupply;
+
+    mapping(address => uint256) public override balanceOf;
+
+    mapping(address => mapping(address => uint256)) public override allowance;
+
+    function transfer(address recipient, uint256 amount)
+        external
+        override
+        returns (bool)
+    {
+        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+
+        balanceOf[msg.sender] -= amount;
+
+        balanceOf[recipient] += amount;
+
+        emit Transfer(msg.sender, recipient, amount);
+
+        return true;
+    }
+
+    function approve(address spender, uint256 amount)
+        external
+        override
+        returns (bool)
+    {
+        allowance[msg.sender][spender] = amount;
+
+        emit Approval(msg.sender, spender, amount);
+
+        return true;
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external override returns (bool) {
+        require(balanceOf[sender] >= amount, "Insufficient balance");
+
+        require(allowance[sender][msg.sender] >= amount, "Allowance exceeded");
+
+        balanceOf[sender] -= amount;
+
+        balanceOf[recipient] += amount;
+
+        allowance[sender][msg.sender] -= amount;
+
+        emit Transfer(sender, recipient, amount);
+
+        return true;
+    }
+
+    function mint(uint256 amount) external {
+        balanceOf[msg.sender] += amount;
+
+        totalSupply += amount;
+
+        emit Transfer(address(0), msg.sender, amount);
+    }
+
+    function burn(uint256 amount) external {
+        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+
+        balanceOf[msg.sender] -= amount;
+
+        totalSupply -= amount;
+
+        emit Transfer(msg.sender, address(0), amount);
+    }
+}
